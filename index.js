5fddab22fd88e964096e7e4e6ec7f8ad63dca503
gingerchris <chrisdowling84@gmail.com>
2021-11-22 12:11:44 +0000

    Initial commit
    Adds markup, css and js to show a button which selects from a list
    of names when clicked.

---
+++ index.js
@@ -0,0 +1,13 @@
+const names = ['Em', 'Emilia'];
+
+const button = document.getElementById('chooser');
+const selectionContainer = document.getElementById('selection');
+const result = document.querySelector('.hidden');
+const box = document.querySelector('.box');
+
+button.addEventListener('click', () => {
+  const name = names[Math.floor(Math.random() * names.length)];
+  selectionContainer.innerHTML = name;
+  result.classList.remove('hidden');
+  box.classList.add('hidden');
+});
